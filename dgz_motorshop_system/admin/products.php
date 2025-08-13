344aee9c854377c54967c26ac56124447078c675
CBetito <betito.309810@ortigas-cainta.sti.edu.ph>
2025-08-13 11:13:34 +0800

    Products design fixed no sidebar button

--- dgz_motorshop_system/admin/products.php
+++ dgz_motorshop_system/admin/products.php
@@ -23,7 +23,7 @@ $products = $pdo->query('SELECT * FROM products')->fetchAll();
     <title>Products</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/style.css">
-    <link rel="stylesheet" href="../assets/dashboard.css">
+    
     <link rel="stylesheet" href="../assets/products.css">
     
 </head>
