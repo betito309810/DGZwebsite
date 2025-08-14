0ac0a84c2ff2dae98a878f718c255c72f1ffcdcb
CBetito <betito.309810@ortigas-cainta.sti.edu.ph>
2025-08-14 23:38:28 +0800

    Edit products

--- dgz_motorshop_system/admin/products.php
+++ dgz_motorshop_system/admin/products.php
@@ -24,7 +24,6 @@ $products = $pdo->query('SELECT * FROM products')->fetchAll();
     <title>Products</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/style.css">
-    
     <link rel="stylesheet" href="../assets/products.css">
     
 </head>
