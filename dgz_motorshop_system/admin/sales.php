f9c0ab3a7338e2564329d228f52f4b1503949862
CBetito <betito.309810@ortigas-cainta.sti.edu.ph>
2025-08-15 00:08:35 +0800

    editing the sales page

--- dgz_motorshop_system/admin/sales.php
+++ dgz_motorshop_system/admin/sales.php
@@ -12,9 +12,11 @@ $orders = $pdo->query($sql)->fetchAll();
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/
-    <title>Sales</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="../assets/sales.css">
+    <title>Sales</title>
+    
 </head>
 
 <body>
